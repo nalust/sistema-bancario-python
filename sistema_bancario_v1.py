52ec448c51d62772cbec980000d963ba41e75125
nalust <analuisasousasantos@gmail.com>
2023-11-17 14:37:14 -0300

    primeira versão do sistema bancário finalizada.

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -12,10 +12,9 @@ Escolha uma das opções:
 '''
 
 saldo = 0
-deposito = 0
 quantidade_saque = 0
 limite_saque_diario = 0
-saque = 0
+extrato = ''
 
 while True:
     option = input(menu)
@@ -23,47 +22,40 @@ while True:
     if option == '1':
         print(' Extrato '.center(20, '*'))
 
-        if deposito == 0 and saque == 0:
+        if extrato == '':
             print(f'''Não foram realizadas movimentações.
-                Saldo atual: {saldo}''')
-        else:
-           
-            print(f'''
-            Depósito: R${deposito}
-            Saque: R${saque}
 
-            Saldo: R${saldo}
-            ''')
+                Saldo: R$ {saldo:.2f}''')
+        else:
+            print(extrato)
+            print(f'Saldo: R$ {saldo:.2f}')
 
     elif option == '2':
         print(' Depósito '.center(20, '*'))
         valor_deposito = float(input('Qual valor deseja depositar? '))
         saldo += valor_deposito
         print('Depósito realizado com sucesso!')
-        deposito += valor_deposito
+        extrato += f'Depósito: R$ {valor_deposito:.2f}\n'
 
     elif option == '3':
         print(' Saque '.center(20, '*'))
         
-        if limite_saque_diario <= 1500:
-            valor_saque = float(input('Informe o valor do saque: '))
-            if valor_saque <= 500:
-                if saldo >= valor_saque:
-                    if quantidade_saque < 3:
-                        limite_saque_diario += valor_saque
-                        saldo -= valor_saque
-                        print('Saque realizado com sucesso!')
-                        quantidade_saque += 1
-                        saque += valor_saque
-                    else:
-                        print('O limite é de 3 (três) saques por dia.')
+        valor_saque = float(input('Informe o valor do saque: '))
+        if valor_saque <= 500:
+            if saldo >= valor_saque:
+                if quantidade_saque < 3:
+                    limite_saque_diario += valor_saque
+                    saldo -= valor_saque
+                    print('Saque realizado com sucesso!')
+                    quantidade_saque += 1
+                    extrato += f'Saque: R$ {valor_saque:.2f}\n'
                 else:
-                    print('Saldo insuficiente.')
+                    print('O limite é de 3 (três) saques por dia.')
             else:
-                print('O limite por saque é de R$ 500.')
+                print('Saldo insuficiente.')
         else:
-            print('O limite de saque diário é de R$ 1.500.')
-            
+            print('O limite por saque é de R$ 500.')
+       
     elif option == '0':
         print('Obrigado por utilizar nossos serviços!')
         break
